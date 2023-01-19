2ea600f50015f22018224e715a0e2ca616fa70d3
hassanjavedd2006 <115046981+hassanjavedd2006@users.noreply.github.com>
2023-01-20 02:05:56 +0500

    Add files via upload

---
+++ atcforhome.js
@@ -0,0 +1,211 @@
+let empty = document.getElementById("dd-sc0");
+let prd1 = document.getElementById("dd-sc1");
+let prd2 = document.getElementById("dd-sc2");
+let prd3 = document.getElementById("dd-sc3");
+let prd4 = document.getElementById("dd-sc4");
+let prd5 = document.getElementById("dd-sc5");
+let prd6 = document.getElementById("dd-sc6");
+let prd7 = document.getElementById("dd-sc7");
+let prd8 = document.getElementById("dd-sc8");
+let prd9 = document.getElementById("dd-sc9");
+
+let total = document.getElementById("total");
+let tot = document.getElementById("dd-sct");
+
+let checkout = document.getElementById("checkout-btn");
+
+let calculation = 0
+let itemnum = 0
+
+
+
+
+function atc1() {
+    itemnum++
+    prd1.innerHTML = "TOSHIBA HAORI - 1600$ - " + itemnum 
+    calculation += 1600
+    total.innerHTML = calculation + "$"
+    prd1.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+
+}
+function atcc1() {
+    itemnum++
+    prd1.innerHTML = "TOSHIBA HAORI - 1600$ - " + itemnum 
+    calculation += 1600
+    total.innerHTML = calculation + "$"
+    prd1.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc2() {
+    itemnum++
+    prd2.innerHTML = "TOSHIBA SEIYA – R32 - 510$ - " + itemnum 
+    calculation += 510
+    total.innerHTML = calculation + "$"
+    prd2.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc2() {
+    itemnum++
+    prd2.innerHTML = "TOSHIBA SEIYA – R32 - 510$ - " + itemnum 
+    calculation += 510
+    total.innerHTML = calculation + "$"
+    prd2.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc3() {
+    itemnum++
+    prd3.innerHTML = "SHORAI PREMIUM – R32 - 1590$ - " + itemnum 
+    calculation += 1590
+    total.innerHTML = calculation + "$"
+    prd3.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc3() {
+    itemnum++
+    prd3.innerHTML = "SHORAI PREMIUM – R32 - 1590$ - " + itemnum 
+    calculation += 1590
+    total.innerHTML = calculation + "$"
+    prd3.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc4() {
+    itemnum++
+    prd4.innerHTML = "SAMSUNG, CAC, 360 Cassette- 290$ - " + itemnum 
+    calculation += 290
+    total.innerHTML = calculation + "$"
+    prd4.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc4() {
+    itemnum++
+    prd4.innerHTML = "SAMSUNG, CAC, 360 Cassette- 290$ - " + itemnum 
+    calculation += 290
+    total.innerHTML = calculation + "$"
+    prd4.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc5() {
+    itemnum++
+    prd5.innerHTML = "SAMSUNG, DVM, 4-Way - 1200$ - " + itemnum 
+    calculation += 1200
+    total.innerHTML = calculation + "$"
+    prd5.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc5() {
+    itemnum++
+    prd5.innerHTML = "SAMSUNG, DVM, 4-Way - 1200$ - " + itemnum 
+    calculation += 1200
+    total.innerHTML = calculation + "$"
+    prd5.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc6() {
+    itemnum++
+    prd6.innerHTML = "SAMSUNG, DVM, 2-Way - 600$ - " + itemnum 
+    calculation += 600
+    total.innerHTML = calculation + "$"
+    prd6.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc6() {
+    itemnum++
+    prd6.innerHTML = "SAMSUNG, DVM, 2-Way - 600$ - " + itemnum 
+    calculation += 600
+    total.innerHTML = calculation + "$"
+    prd6.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc7() {
+    itemnum++
+    prd7.innerHTML = "LG, 12,000 BTU - 520$ - " + itemnum 
+    calculation += 520
+    total.innerHTML = calculation + "$"
+    prd7.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc7() {
+    itemnum++
+    prd7.innerHTML = "LG, 12,000 BTU - 520$ - " + itemnum 
+    calculation += 520
+    total.innerHTML = calculation + "$"
+    prd7.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc8() {
+    itemnum++
+    prd8.innerHTML = "LG, 23,500 BTU - 700$ - " + itemnum 
+    calculation += 700
+    total.innerHTML = calculation + "$"
+    prd8.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc8() {
+    itemnum++
+    prd8.innerHTML = "LG, 23,500 BTU - 700$ - " + itemnum 
+    calculation += 700
+    total.innerHTML = calculation + "$"
+    prd8.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+
+function atc9() {
+    itemnum++
+    prd9.innerHTML = "LG, 14,000 BTU - 1590$ - " + itemnum 
+    calculation += 570
+    total.innerHTML = calculation + "$"
+    prd9.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
+function atcc9() {
+    itemnum++
+    prd9.innerHTML = "LG, 14,000 BTU - 1590$ - " + itemnum
+    calculation += 570
+    total.innerHTML = calculation + "$"
+    prd9.style.display = "block"
+    tot.style.display = "block"
+    checkout.style.display = "block"
+    empty.style.display = "none"
+}
---
+++ rating.js
@@ -0,0 +1,490 @@
+let startext1th = document.getElementById("rating_txt1th");
+let startext2th = document.getElementById("rating_txt2th");
+let startext3th = document.getElementById("rating_txt3th");
+let startext4th = document.getElementById("rating_txt4th");
+let startext5th = document.getElementById("rating_txt5th");
+let val_star1 = 4;
+let val_star2 = 6;
+let val_star3 = 10;
+let val_star4 = 12;
+let val_star5 = 68;
+
+function star1th() {
+    val_star1++;
+    startext1th.innerHTML = "(" + val_star1 + ")";
+}
+function star2th() {
+    val_star2++;
+    startext2th.innerHTML = "(" + val_star2 + ")";
+}
+function star3th() {
+    val_star3++;
+    startext3th.innerHTML = "(" + val_star3 + ")";
+}
+function star4th() {
+    val_star4++;
+    startext4th.innerHTML = "(" + val_star4 + ")";
+}
+function star5th() {
+    val_star5++;
+    startext5th.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmth = document.getElementById("rating_txt1fmth");
+let startext2fmth = document.getElementById("rating_txt2fmth");
+let startext3fmth = document.getElementById("rating_txt3fmth");
+let startext4fmth = document.getElementById("rating_txt4fmth");
+let startext5fmth = document.getElementById("rating_txt5fmth");
+
+function starfm1th() {
+    val_star1++;
+    startext1fmth.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2th() {
+    val_star2++;
+    startext2fmth.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3th() {
+    val_star3++;
+    startext3fmth.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4th() {
+    val_star4++;
+    startext4fmth.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5th() {
+    val_star5++;
+    startext5fmth.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1ts = document.getElementById("rating_txt1ts");
+let startext2ts = document.getElementById("rating_txt2ts");
+let startext3ts = document.getElementById("rating_txt3ts");
+let startext4ts = document.getElementById("rating_txt4ts");
+let startext5ts = document.getElementById("rating_txt5ts");
+
+function star1ts() {
+    val_star1++;
+    startext1ts.innerHTML = "(" + val_star1 + ")";
+}
+function star2ts() {
+    val_star2++;
+    startext2ts.innerHTML = "(" + val_star2 + ")";
+}
+function star3ts() {
+    val_star3++;
+    startext3ts.innerHTML = "(" + val_star3 + ")";
+}
+function star4ts() {
+    val_star4++;
+    startext4ts.innerHTML = "(" + val_star4 + ")";
+}
+function star5ts() {
+    val_star5++;
+    startext5ts.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmts = document.getElementById("rating_txt1fmts");
+let startext2fmts = document.getElementById("rating_txt2fmts");
+let startext3fmts = document.getElementById("rating_txt3fmts");
+let startext4fmts = document.getElementById("rating_txt4fmts");
+let startext5fmts = document.getElementById("rating_txt5fmts");
+
+function starfm1ts() {
+    val_star1++;
+    startext1fmts.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2ts() {
+    val_star2++;
+    startext2fmts.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3ts() {
+    val_star3++;
+    startext3fmts.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4ts() {
+    val_star4++;
+    startext4fmts.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5ts() {
+    val_star5++;
+    startext5fmts.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1tsp = document.getElementById("rating_txt1tsp");
+let startext2tsp = document.getElementById("rating_txt2tsp");
+let startext3tsp = document.getElementById("rating_txt3tsp");
+let startext4tsp = document.getElementById("rating_txt4tsp");
+let startext5tsp = document.getElementById("rating_txt5tsp");
+
+function star1tsp() {
+    val_star1++;
+    startext1tsp.innerHTML = "(" + val_star1 + ")";
+}
+function star2tsp() {
+    val_star2++;
+    startext2tsp.innerHTML = "(" + val_star2 + ")";
+}
+function star3tsp() {
+    val_star3++;
+    startext3tsp.innerHTML = "(" + val_star3 + ")";
+}
+function star4tsp() {
+    val_star4++;
+    startext4tsp.innerHTML = "(" + val_star4 + ")";
+}
+function star5tsp() {
+    val_star5++;
+    startext5tsp.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmtsp = document.getElementById("rating_txt1fmtsp");
+let startext2fmtsp = document.getElementById("rating_txt2fmtsp");
+let startext3fmtsp = document.getElementById("rating_txt3fmtsp");
+let startext4fmtsp = document.getElementById("rating_txt4fmtsp");
+let startext5fmtsp = document.getElementById("rating_txt5fmtsp");
+
+function starfm1tsp() {
+    val_star1++;
+    startext1fmtsp.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2tsp() {
+    val_star2++;
+    startext2fmtsp.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3tsp() {
+    val_star3++;
+    startext3fmtsp.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4tsp() {
+    val_star4++;
+    startext4fmtsp.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5tsp() {
+    val_star5++;
+    startext5fmtsp.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1c360 = document.getElementById("rating_txt1c360");
+let startext2c360 = document.getElementById("rating_txt2c360");
+let startext3c360 = document.getElementById("rating_txt3c360");
+let startext4c360 = document.getElementById("rating_txt4c360");
+let startext5c360 = document.getElementById("rating_txt5c360");
+
+function star1c360() {
+    val_star1++;
+    startext1c360.innerHTML = "(" + val_star1 + ")";
+}
+function star2c360() {
+    val_star2++;
+    startext2c360.innerHTML = "(" + val_star2 + ")";
+}
+function star3c360() {
+    val_star3++;
+    startext3c360.innerHTML = "(" + val_star3 + ")";
+}
+function star4c360() {
+    val_star4++;
+    startext4c360.innerHTML = "(" + val_star4 + ")";
+}
+function star5c360() {
+    val_star5++;
+    startext5c360.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmc360 = document.getElementById("rating_txt1fmc360");
+let startext2fmc360 = document.getElementById("rating_txt2fmc360");
+let startext3fmc360 = document.getElementById("rating_txt3fmc360");
+let startext4fmc360 = document.getElementById("rating_txt4fmc360");
+let startext5fmc360 = document.getElementById("rating_txt5fmc360");
+
+function starfm1c360() {
+    val_star1++;
+    startext1fmc360.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2c360() {
+    val_star2++;
+    startext2fmc360.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3c360() {
+    val_star3++;
+    startext3fmc360.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4c360() {
+    val_star4++;
+    startext4fmc360.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5c360() {
+    val_star5++;
+    startext5fmc360.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1dvm4 = document.getElementById("rating_txt1dvm4");
+let startext2dvm4 = document.getElementById("rating_txt2dvm4");
+let startext3dvm4 = document.getElementById("rating_txt3dvm4");
+let startext4dvm4 = document.getElementById("rating_txt4dvm4");
+let startext5dvm4 = document.getElementById("rating_txt5dvm4");
+
+function star1dvm4() {
+    val_star1++;
+    startext1dvm4.innerHTML = "(" + val_star1 + ")";
+}
+function star2dvm4() {
+    val_star2++;
+    startext2dvm4.innerHTML = "(" + val_star2 + ")";
+}
+function star3dvm4() {
+    val_star3++;
+    startext3dvm4.innerHTML = "(" + val_star3 + ")";
+}
+function star4dvm4() {
+    val_star4++;
+    startext4dvm4.innerHTML = "(" + val_star4 + ")";
+}
+function star5dvm4() {
+    val_star5++;
+    startext5dvm4.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmdvm4 = document.getElementById("rating_txt1fmdvm4");
+let startext2fmdvm4 = document.getElementById("rating_txt2fmdvm4");
+let startext3fmdvm4 = document.getElementById("rating_txt3fmdvm4");
+let startext4fmdvm4 = document.getElementById("rating_txt4fmdvm4");
+let startext5fmdvm4 = document.getElementById("rating_txt5fmdvm4");
+
+function starfm1dvm4() {
+    val_star1++;
+    startext1fmdvm4.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2dvm4() {
+    val_star2++;
+    startext2fmdvm4.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3dvm4() {
+    val_star3++;
+    startext3fmdvm4.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4dvm4() {
+    val_star4++;
+    startext4fmdvm4.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5dvm4() {
+    val_star5++;
+    startext5fmdvm4.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1dvm2 = document.getElementById("rating_txt1dvm2");
+let startext2dvm2 = document.getElementById("rating_txt2dvm2");
+let startext3dvm2 = document.getElementById("rating_txt3dvm2");
+let startext4dvm2 = document.getElementById("rating_txt4dvm2");
+let startext5dvm2 = document.getElementById("rating_txt5dvm2");
+
+function star1dvm2() {
+    val_star1++;
+    startext1dvm2.innerHTML = "(" + val_star1 + ")";
+}
+function star2dvm2() {
+    val_star2++;
+    startext2dvm2.innerHTML = "(" + val_star2 + ")";
+}
+function star3dvm2() {
+    val_star3++;
+    startext3dvm2.innerHTML = "(" + val_star3 + ")";
+}
+function star4dvm2() {
+    val_star4++;
+    startext4dvm2.innerHTML = "(" + val_star4 + ")";
+}
+function star5dvm2() {
+    val_star5++;
+    startext5dvm2.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmdvm2 = document.getElementById("rating_txt1fmdvm2");
+let startext2fmdvm2 = document.getElementById("rating_txt2fmdvm2");
+let startext3fmdvm2 = document.getElementById("rating_txt3fmdvm2");
+let startext4fmdvm2 = document.getElementById("rating_txt4fmdvm2");
+let startext5fmdvm2 = document.getElementById("rating_txt5fmdvm2");
+
+function starfm1dvm2() {
+    val_star1++;
+    startext1fmdvm2.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2dvm2() {
+    val_star2++;
+    startext2fmdvm2.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3dvm2() {
+    val_star3++;
+    startext3fmdvm2.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4dvm2() {
+    val_star4++;
+    startext4fmdvm2.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5dvm2() {
+    val_star5++;
+    startext5fmdvm2.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1lgb12 = document.getElementById("rating_txt1lgb12");
+let startext2lgb12 = document.getElementById("rating_txt2lgb12");
+let startext3lgb12 = document.getElementById("rating_txt3lgb12");
+let startext4lgb12 = document.getElementById("rating_txt4lgb12");
+let startext5lgb12 = document.getElementById("rating_txt5lgb12");
+
+function star1lgb12() {
+    val_star1++;
+    startext1lgb12.innerHTML = "(" + val_star1 + ")";
+}
+function star2lgb12() {
+    val_star2++;
+    startext2lgb12.innerHTML = "(" + val_star2 + ")";
+}
+function star3lgb12() {
+    val_star3++;
+    startext3lgb12.innerHTML = "(" + val_star3 + ")";
+}
+function star4lgb12() {
+    val_star4++;
+    startext4lgb12.innerHTML = "(" + val_star4 + ")";
+}
+function star5lgb12() {
+    val_star5++;
+    startext5lgb12.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmlgb12 = document.getElementById("rating_txt1fmlgb12");
+let startext2fmlgb12 = document.getElementById("rating_txt2fmlgb12");
+let startext3fmlgb12 = document.getElementById("rating_txt3fmlgb12");
+let startext4fmlgb12 = document.getElementById("rating_txt4fmlgb12");
+let startext5fmlgb12 = document.getElementById("rating_txt5fmlgb12");
+
+function starfm1lgb12() {
+    val_star1++;
+    startext1fmlgb12.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2lgb12() {
+    val_star2++;
+    startext2fmlgb12.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3lgb12() {
+    val_star3++;
+    startext3fmlgb12.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4lgb12() {
+    val_star4++;
+    startext4fmlgb12.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5lgb12() {
+    val_star5++;
+    startext5fmlgb12.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1lgb23 = document.getElementById("rating_txt1lgb23");
+let startext2lgb23 = document.getElementById("rating_txt2lgb23");
+let startext3lgb23 = document.getElementById("rating_txt3lgb23");
+let startext4lgb23 = document.getElementById("rating_txt4lgb23");
+let startext5lgb23 = document.getElementById("rating_txt5lgb23");
+
+function star1lgb23() {
+    val_star1++;
+    startext1lgb23.innerHTML = "(" + val_star1 + ")";
+}
+function star2lgb23() {
+    val_star2++;
+    startext2lgb23.innerHTML = "(" + val_star2 + ")";
+}
+function star3lgb23() {
+    val_star3++;
+    startext3lgb23.innerHTML = "(" + val_star3 + ")";
+}
+function star4lgb23() {
+    val_star4++;
+    startext4lgb23.innerHTML = "(" + val_star4 + ")";
+}
+function star5lgb23() {
+    val_star5++;
+    startext5lgb23.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmlgb23 = document.getElementById("rating_txt1fmlgb23");
+let startext2fmlgb23 = document.getElementById("rating_txt2fmlgb23");
+let startext3fmlgb23 = document.getElementById("rating_txt3fmlgb23");
+let startext4fmlgb23 = document.getElementById("rating_txt4fmlgb23");
+let startext5fmlgb23 = document.getElementById("rating_txt5fmlgb23");
+
+function starfm1lgb23() {
+    val_star1++;
+    startext1fmlgb23.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2lgb23() {
+    val_star2++;
+    startext2fmlgb23.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3lgb23() {
+    val_star3++;
+    startext3fmlgb23.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4lgb23() {
+    val_star4++;
+    startext4fmlgb23.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5lgb23() {
+    val_star5++;
+    startext5fmlgb23.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1lgb14 = document.getElementById("rating_txt1lgb14");
+let startext2lgb14 = document.getElementById("rating_txt2lgb14");
+let startext3lgb14 = document.getElementById("rating_txt3lgb14");
+let startext4lgb14 = document.getElementById("rating_txt4lgb14");
+let startext5lgb14 = document.getElementById("rating_txt5lgb14");
+
+function star1lgb14() {
+    val_star1++;
+    startext1lgb14.innerHTML = "(" + val_star1 + ")";
+}
+function star2lgb14() {
+    val_star2++;
+    startext2lgb14.innerHTML = "(" + val_star2 + ")";
+}
+function star3lgb14() {
+    val_star3++;
+    startext3lgb14.innerHTML = "(" + val_star3 + ")";
+}
+function star4lgb14() {
+    val_star4++;
+    startext4lgb14.innerHTML = "(" + val_star4 + ")";
+}
+function star5lgb14() {
+    val_star5++;
+    startext5lgb14.innerHTML = "(" + val_star5 + ")";
+}
+
+let startext1fmlgb14 = document.getElementById("rating_txt1fmlgb14");
+let startext2fmlgb14 = document.getElementById("rating_txt2fmlgb14");
+let startext3fmlgb14 = document.getElementById("rating_txt3fmlgb14");
+let startext4fmlgb14 = document.getElementById("rating_txt4fmlgb14");
+let startext5fmlgb14 = document.getElementById("rating_txt5fmlgb14");
+
+function starfm1lgb14() {
+    val_star1++;
+    startext1fmlgb14.innerHTML = "(" + val_star1 + ")";
+}
+function starfm2lgb14() {
+    val_star2++;
+    startext2fmlgb14.innerHTML = "(" + val_star2 + ")";
+}
+function starfm3lgb14() {
+    val_star3++;
+    startext3fmlgb14.innerHTML = "(" + val_star3 + ")";
+}
+function starfm4lgb14() {
+    val_star4++;
+    startext4fmlgb14.innerHTML = "(" + val_star4 + ")";
+}
+function starfm5lgb14() {
+    val_star5++;
+    startext5fmlgb14.innerHTML = "(" + val_star5 + ")";
+}
